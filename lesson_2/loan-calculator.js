4afcaa2addedc822c98e28991bc8542bd8ce9347
Michael Singhurse <michaelsinghurse@gmail.com>
2020-01-16 22:40:26 +0000

    finished exercises

---
+++ lesson_2/loan-calculator.js
@@ -0,0 +1,94 @@
+// loan-calculator.js
+
+let rlSync = require('readline-sync');
+
+greetUser();
+
+let principal = getPrincipal();
+
+let apr = getAPR();
+
+let termYears = getTermInYears();
+
+let monthlyPayment = calculateMonthlyPayment(principal, apr, termYears);
+
+printResults(principal, apr, termYears, monthlyPayment);
+
+function calculateMonthlyPayment(prin, apr, term) {
+
+  let monthlyRate = apr / 100 / 12;
+  let termInMonths = term * 12;
+  let monthlyPayment;
+
+  if (monthlyRate === 0) {
+    monthlyPayment = prin / termInMonths;
+  } else {
+    monthlyPayment = prin * (monthlyRate /
+      (1 - Math.pow((1 + monthlyRate),(-termInMonths))));
+  }
+
+  return monthlyPayment.toFixed(2);
+}
+
+function getAPR() {
+
+  console.log('What is the APR (Annual Percentage Rate)?\n' +
+    'For example, if the rate is 5.5%, enter 5.5');
+
+  let rate = Number(rlSync.question());
+
+  while (Number.isNaN(rate) || rate < 0) {
+    console.log('Huh. Something\'s wrong. Please don\'t include the percent ' +
+      'symbol or enter a negative rate.');
+
+    rate = rlSync.question();
+  }
+
+  return rate;
+}
+
+function getPrincipal() {
+
+  console.log('What is the loan amount in dollars and cents?');
+
+  let principal = Number(rlSync.question());
+
+  while (Number.isNaN(principal) || principal <= 0) {
+    console.log('Huh. That input didn\'t look right. Please provide a positive ' +
+      'number without the dollar sign.');
+
+    principal = Number(rlSync.question());
+  }
+
+  return principal;
+}
+
+function getTermInYears() {
+
+  console.log('And what is the loan duration in years?');
+
+  let term = Number(rlSync.question());
+
+  while (!Number.isInteger(term) || term < 0) {
+
+    console.log('Hmm. Something is wrong. The duration most be in whole ' +
+      'number of years (no decimals) and can\'t be negative or zero');
+
+    term = Number(rlSync.question());
+  }
+
+  return term;
+}
+
+function greetUser() {
+  console.log('Welcome to the Loan Calculator!');
+}
+
+function printResults(principal, apr, term, payment) {
+    console.log('For the following terms...\n' +
+      '\tAmount: $' + principal + '\n' +
+      '\tAPR: ' + apr + '%\n' +
+      '\tDuration: ' + term + ' years\n' +
+      'Your monthly payment is: $' + payment);
+}
+
